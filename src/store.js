e7e761aa466d77316ca497477830e353ab021fcd
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-04-22 22:42:24 +0700

    store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-param-reassign */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import db from './api/firebase';
+import { async } from 'q';
 
 Vue.use(Vuex);
 
@@ -19,6 +23,172 @@ export default new Vuex.Store({
         plain: '',
       },
     ],
+    rooms: [],
+    room: '',
+    quiz: {
+      allQuiz: [],
+      oneQuiz: '',
+    },
+    user: {
+      name: '',
+      score: 0,
+    },
+  },
+  mutations: {
+    changeAllQuiz(state, payload) {
+      if (payload.length === 0) {
+        state.quiz.allQuiz = [];
+      } else {
+        state.quiz.allQuiz = payload.results;
+      }
+    },
+    changeOneQuiz(state, payload) {
+      if (payload === '') {
+        state.quiz.oneQuiz = '';
+      } else {
+        state.quiz.oneQuiz = state.quiz.allQuiz[payload];
+      }
+    },
+    changeRooms(state, payload) {
+      if (payload === []) {
+        state.rooms = [];
+      } else {
+        state.rooms.push(payload);
+      }
+    },
+    changeRoom(state, payload) {
+      if (payload === '') {
+        state.room = '';
+      } else {
+        state.room = payload;
+      }
+    }
+  },
+  actions: {
+    getAllQuiz(context) {
+      context.commit('changeAllQuiz', []);
+      axios
+        .get('https://opentdb.com/api.php?amount=50&type=boolean&difficulty=hard')
+        .then((response) => {
+          context.commit('changeAllQuiz', response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getOneQuiz(context) {
+      context.commit('changeOneQuiz', '');
+      const random = Math.floor(Math.random() * 51);
+      context.commit('changeOneQuiz', random);
+    },
+    joinRoom(context, payload) {
+      db
+        .collection('rooms')
+        .doc(payload.id)
+        .set(
+          { players: [{ name: payload.name, score: 0 }] },
+          { merge: true },
+        )
+        .then((response) => {
+          if (response !== null) {
+            context.dispatch('getAllRooms');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    createUser(context, payload) {
+      db
+        .collection('user')
+        .add({
+          name: payload.name,
+          score: 0,
+          status: 'idle',
+        })
+        .then((response) => {
+          localStorage.setItem('id', response.id);
+          localStorage.setItem('name', response.name);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    createRoom(context, payload) {
+      db
+        .collection('room')
+        .add({
+          status: 'idle',
+          players: [{ userId: localStorage.getItem('id') }],
+          key: payload.key,
+        })
+        .then((response) => {
+          this.$router.push(`/rooms/${response.id}`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getAllRooms(context) {
+      context.commit('changeRooms', []);
+      db
+        .collection('room')
+      // .get()
+      // .then((data) => {
+      //   console.log(data[0].data())
+      // })
+      // .onSnapshot(async (querySnapshot) => {
+      //   // const result = await Promise.all(
+      //   //   querySnapshot.docs.map(async (e) => {
+      //   //     const data = { id: e.id, ...e.data() }
+      //   //     data.user = await db.collection('user').doc(data.userId).get();
+      //   //     return data;
+      //   //   }),
+      //   // );
+      //   console.log(querySnapshot);
+      //   // context.commit('changeRooms', querySnapshot);
+      //   // querySnapshot.forEach((doc) => {
+      //   //   const newData = {
+      //   //     id: doc.id,
+      //   //     ...doc.data(),
+      //   //   };
+      //   // console.log(newData);
+      //   // context.commit('changeRooms', newData);
+      //   // });
+      // });
+    },
+    getOneRoom(context, id) {
+      context.commit('changeRoom', '');
+      db
+        .collection('room')
+        .doc(id)
+        .onSnapshot((querySnapshot) => {
+          console.log(querySnapshot);
+          context.commit('changeRoom', querySnapshot.data());
+        });
+    },
+    startGame(context, id) {
+      db
+        .collection('room')
+        .doc(id)
+        .update({
+          status: 'active',
+        })
+        .then((response) => {
+          return db
+            .collection('user')
+            .doc(localStorage.getItem('id'))
+            .update({
+              status: 'active',
+            });
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   mutations: {},
   actions: {},
---
+++ src/views/BoardPlay.vue
@@ -0,0 +1,44 @@
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import db from "@/api/firebase.vue";
+import axios from "axios";
+export default {
+  name: "allTasks",
+  components: {
+    card
+  },
+  data() {
+    return {
+      allQuestion: [],
+      oneQuestion: ""
+    };
+  },
+  methods: {
+    getAllQuiz() {
+      this.allQuestion = [];
+      axios
+        .get(
+          "https://opentdb.com/api.php?amount=50&type=boolean&difficulty=hard"
+        )
+        .then(({ data }) => {
+          this.allQuestion = data.result;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    randomQuestion() {
+      this.oneQuestion = "";
+      let index = Math.floor(Math.random() * 51);
+      this.oneQuestion = this.allQuestion[index];
+    }
+  }
+};
+</script>
