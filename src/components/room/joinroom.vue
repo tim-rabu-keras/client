a6486338c155264e46029ff7f3d46f047e1c132d
Muhammad <myhoomac@hotmail.com>
2019-04-23 09:55:55 +0700

    the room

--- src/components/room/joinroom.vue
+++ src/components/room/joinroom.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <a-scene>
     <a-assets>
-      <img id=""skyy" src="sky4" alt="a sky">
-      <img src="" alt="a floor" />
+      <img id="skyy" src="../../assets/flooor3.jpg" alt="a sky" />
+      <img id="floorthree" src="../../assets/night.jpg" alt="a floor" />
     </a-assets>
   </a-scene>
 </template>
